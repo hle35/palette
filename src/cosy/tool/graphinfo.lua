c6141fe8162e25174c56f433b8498deac83192bf
Alban Linard <alban@linard.fr>
2016-02-05 11:26:46 +0100

    Create graphinfo tool.

---
+++ src/cosy/tool/graphinfo.lua
@@ -0,0 +1,42 @@
+return function (Layer)
+
+  local labels  = Layer.key.labels
+  local meta    = Layer.key.meta
+  local refines = Layer.key.refines
+  local tool    = Layer.require "cosy/formalism/tool"
+
+  local graphinfo = Layer.new {
+    name = "cosy/tool/graphinfo",
+  }
+
+  graphinfo [labels] = {
+    ["cosy/tool/graphinfo"] = true
+  }
+  local _ = Layer.reference "cosy/tool/graphinfo"
+
+  graphinfo [refines] = {
+    tool
+  }
+
+  graphinfo.description = "Computes #vertices and #edges of a graph."
+
+  graphinfo.graph = {
+    [refines]   = { _ [meta].parameter_type },
+    name        = "graph",
+    description = "graph to analyze",
+    default     = nil,
+  }
+
+  graphinfo.run = function (options)
+    local model = options.model.graph
+    model.nb_vertices = 0
+    model.nb_edges    = 0
+    for _ in pairs (model.vertices) do
+      model.nb_vertices = model.nb_vertices+1
+    end
+    for _ in pairs (model.edges) do
+      model.nb_edges = model.nb_edges+1
+    end
+  end
+
+end
